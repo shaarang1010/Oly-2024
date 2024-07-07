72843ca5365085841ead47541b61ac444eadbf55
Shaarang T <shaarang.1910@gmail.com>
2024-07-07 21:52:27 +1000

    define struct

--- main.go
+++ main.go
@@ -4,6 +4,22 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Schedule struct {
+	Date   string
+	Sport  *sport
+	Events []*Event
+}
+
+type Event struct {
+	Time        string
+	Description string
+	Location    string
+}
+type sport struct {
+	Name string
+	Link string
+}
+
 func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("en.wikipedia.org"),
